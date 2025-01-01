f3a30b37f4466ea35059abb3c4a199d0432ab195
anujkharbash2 <anujkharbash2@gmail.com>
2025-01-01 22:07:47 +0530

    update forms

--- javascript/script.js
+++ javascript/script.js
@@ -9,8 +9,25 @@
     });
 
 
-    function handleSubmit(event) {
-        event.preventDefault(); // Prevent default form submission
-        alert('Your message has been sent successfully!'); // Show alert to user
-        window.location.href = '/contactus.html'; // Redirect to homepage
-    }
+    // function handleSubmit(event) {
+    //     event.preventDefault(); // Prevent default form submission
+    //     alert('Your message has been sent successfully!'); // Show alert to user
+    //     window.location.href = '/contactus.html'; // Redirect to homepage
+    // }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+      
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+      
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formData).toString()
+        })
+          .then(() => console.log("Form successfully submitted"))
+          .catch(error => alert(error));
+      };
+      
+      document.querySelector("form").addEventListener("submit", handleSubmit);
