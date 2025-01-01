3456bb763b20910e01d1c6f3a42d7c28261e70bf
anujkharbash2 <anujkharbash2@gmail.com>
2025-01-01 21:46:51 +0530

    updating conatctus form on 1jan 2025

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,10 @@
             navLinks.classList.toggle('open'); // Add or remove 'open' class
         });
     });
+
+
+    function handleSubmit(event) {
+        event.preventDefault(); // Prevent default form submission
+        alert('Your message has been sent successfully!'); // Show alert to user
+        window.location.href = '/contactus.html'; // Redirect to homepage
+    }
