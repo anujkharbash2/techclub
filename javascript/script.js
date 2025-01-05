d5d9858274a86c5bac8d551f6b35567f031c2590
Anuj Kumar <anujkharbash2@gmail.com>
2025-01-05 23:11:32 +0530

    js update

--- javascript/script.js
+++ javascript/script.js
@@ -9,38 +9,11 @@
     });
 
 
-    // function handleSubmit(event) {
-    //     event.preventDefault(); // Prevent default form submission
-    //     alert('Your message has been sent successfully!'); // Show alert to user
-    //     window.location.href = '/contactus.html'; 
-    // }
-
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-      
-    //     const myForm = event.target;
-    //     const formData = new FormData(myForm);
-      
-    //     fetch("/", {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //       body: new URLSearchParams(formData).toString()
-    //     })
-    //       .then(() => console.log("Form successfully submitted"))
-    //       .catch(error => alert(error));
-          
-    //         window.location.href = '/contactus.html';
-
-    //   };
-      
-    //   document.querySelector("form").addEventListener("submit", handleSubmit);
-
-
 
 
 
     const handleSubmit = async event => {
-      event.preventDefault(); // Prevent the default form submission behavior
+      event.preventDefault();
   
       const myForm = event.target;
       const formData = new FormData(myForm);
@@ -56,14 +29,14 @@
               throw new Error("Failed to submit the form.");
           }
   
-          // Notify user of successful submission
+          
           alert("Form successfully submitted");
           
-          // Optionally, reset the form and reload the page
-          myForm.reset();  // Reset form fields
-          location.reload();  // Reload the page (if needed)
+          
+          myForm.reset();  
+          location.reload();  
       } catch (error) {
-          alert("Error: " + error.message);  // Show detailed error message
+          alert("Error: " + error.message);  
       }
   };
   
